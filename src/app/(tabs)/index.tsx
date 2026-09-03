d4f4752f5a6943b54272f635fabedcdc027cea0e
wedocreateweb <hiali68955@gmail.com>
2026-09-03 22:19:49 +0500

    feat: project setup, nativewind, routing, tabs navigation

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,23 +1,25 @@
 // src/app/index.tsx
 import { Link } from 'expo-router';
+import { styled } from "nativewind";
 import {
-  Text,
-  View
+  Text
 } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 import "../../../global.css";
 
+const SafeAreaView = styled(RNSafeAreaView)
 export default function HomeScreen() {
 
   return (
-    <View className='flex-1 items-center justify-center bg-background'>
+    <SafeAreaView className='flex-1 bg-background p-5'>
       <Text className='text-xl font-bold text-success'>
         Welcome to Nativewind
       </Text>
       <Link href="/onboarding" className='mt-4 rounded bg-primary text-white p-4'>Go to onboarding</Link>
       <Link href="/(auth)/sign-in" className='mt-4 rounded bg-primary text-white p-4'>Go to Sign In</Link>
       <Link href="/(auth)/sign-up" className='mt-4 rounded bg-primary text-white p-4'>Go to Sign Up</Link>
-      <Link href="/(tabs)/subscriptions/nonsptifyyyy" className='mt-4 rounded bg-primary text-white p-4'>Go to spotifyyyyy</Link>
+      <Link href="/(tabs)/subscriptions/nonsptifyyyy" className='mt-4 rounded bg-primary text-white p-4'>Go to spotify</Link>
       <Link href={{ pathname: "/(tabs)/subscriptions/[id]", params: { id: "Spotify" } }} className='mt-4 rounded bg-primary text-white p-4'>Go to spotifyyyyy</Link>
-    </View>
+    </SafeAreaView>
   );
 }
