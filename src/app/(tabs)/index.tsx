7e3e69a105d35909fdd7cb8d48796182bd16eb30
wedocreateweb <hiali68955@gmail.com>
2026-09-03 23:07:26 +0500

    implement fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -12,14 +12,12 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className='text-xl font-bold text-success'>
-        Welcome to Nativewind
+      <Text className='text-7xl font-sans-extrabold'>
+        Home
       </Text>
-      <Link href="/onboarding" className='mt-4 rounded bg-primary text-white p-4'>Go to onboarding</Link>
-      <Link href="/(auth)/sign-in" className='mt-4 rounded bg-primary text-white p-4'>Go to Sign In</Link>
-      <Link href="/(auth)/sign-up" className='mt-4 rounded bg-primary text-white p-4'>Go to Sign Up</Link>
-      <Link href="/(tabs)/subscriptions/nonsptifyyyy" className='mt-4 rounded bg-primary text-white p-4'>Go to spotify</Link>
-      <Link href={{ pathname: "/(tabs)/subscriptions/[id]", params: { id: "Spotify" } }} className='mt-4 rounded bg-primary text-white p-4'>Go to spotifyyyyy</Link>
+      <Link href="/onboarding" className='mt-4 font-sans-bold  rounded bg-primary text-white p-4'>Go to onboarding</Link>
+      <Link href="/(auth)/sign-in" className='mt-4 font-sans-bold rounded bg-primary text-white p-4'>Go to Sign In</Link>
+      <Link href="/(auth)/sign-up" className='mt-4 font-sans-bold rounded bg-primary text-white p-4'>Go to Sign Up</Link>
     </SafeAreaView>
   );
 }
